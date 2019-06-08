368dee502d3a328c92f234637a3a07d19df90b49
Jeffrey Crewe-Brown <l33tch@gmail.com>
2019-06-08 22:18:55 +0200

    Broad exception handling as input will be well formed

--- leaguer.py
+++ leaguer.py
@@ -5,19 +5,24 @@ from models.game import Game
 
 def main():
     game = Game()
-
     if len(sys.argv) > 1:
         # Read game results from file
-        f = open(sys.argv[1], "r")
-        for x in f:
-            game.register_game(x)
-        f.close()
+        try:
+            f = open(sys.argv[1], "r")
+            for x in f:
+                game.register_game(x)
+            f.close()
+        except:
+            print("Problem reading file")
     else:
         # Read game results from input strings
-        text_in = input()
-        while text_in:
-            game.register_game(text_in)
+        try:
             text_in = input()
+            while text_in:
+                game.register_game(text_in)
+                text_in = input()
+        except:
+            print("Problem reading input")
 
     game.print()
 
