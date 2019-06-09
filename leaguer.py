636e89552512f75aaed5b606b37f8134a689cda3
Jeffrey Crewe-Brown <l33tch@gmail.com>
2019-06-09 17:30:44 +0200

    Added instruction command for manual input mode

--- leaguer.py
+++ leaguer.py
@@ -17,7 +17,8 @@ def main():
     else:
         # Read game results from input strings
         try:
-            text_in = input()
+            text_in = input("Enter game results in the expected format (<team1 score1, team2 score2) "
+                            "and enter a blank line to end and calculate scores:\n")
             while text_in:
                 game.register_game(text_in)
                 text_in = input()
