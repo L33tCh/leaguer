461013084cad817f7066dfe4fcdb43bfbb99c8bc
Jeffrey Crewe-Brown <l33tch@gmail.com>
2019-06-09 08:40:23 +0200

    Mock file input

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,7 +1,7 @@
 import leaguer as app
 import pytest
 from unittest import mock
-from unittest.mock import patch
+from unittest.mock import patch, mock_open
 import io
 
 
@@ -30,9 +30,17 @@ class TestMain:
     @pytest.fixture(params=['', 'input.txt'])
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_file_param(self, mock_stdout):
-        app.main()
-        assert mock_stdout.getvalue() == "1. Tarantulas, 6 pts\n" \
-                                         "2. Lions, 5 pts\n" \
-                                         "3. FC Awesome, 1 pt\n" \
-                                         "3. Snakes, 1 pt\n" \
-                                         "5. Grouches, 0 pts\n"
+        inputs = [
+            "Lions 3, Snakes 3",
+            "Tarantulas 1, FC Awesome 0",
+            "Lions 1, FC Awesome 1",
+            "Tarantulas 3, Snakes 1",
+            "Lions 4, Grouches 0",
+        ]
+        with patch("builtins.open", mock_open(read_data="\n".join(inputs))) as mock_file:
+            app.main()
+            assert mock_stdout.getvalue() == "1. Tarantulas, 6 pts\n" \
+                                             "2. Lions, 5 pts\n" \
+                                             "3. FC Awesome, 1 pt\n" \
+                                             "3. Snakes, 1 pt\n" \
+                                             "5. Grouches, 0 pts\n"
